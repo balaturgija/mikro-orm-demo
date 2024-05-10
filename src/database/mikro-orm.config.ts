c8fc77a9bbaf6983581fb7f37dfa6c0832cb91f6
balaturgija <matej.balat@gmail.com>
2024-05-10 08:53:26 +0200

    Fix configuration

--- src/database/mikro-orm.config.ts
+++ src/database/mikro-orm.config.ts
@@ -1,20 +1,29 @@
 import { Options } from '@mikro-orm/core';
-import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { EntityGenerator } from '@mikro-orm/entity-generator';
+import { Migrator } from '@mikro-orm/migrations';
+import { defineConfig, PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { SeedManager } from '@mikro-orm/seeder';
 import { ConfigService } from '@nestjs/config';
 
 const configService = new ConfigService();
 
-export default {
-  entities: ['dist/src/*/entities/*.entity.js'],
-  entitiesTs: ['src/*/entities/*.entity.ts'],
+export default defineConfig({
+  entities: ['./dist/*/entities/*.entity.js'],
+  entitiesTs: ['./src/*/entities/*.entity.ts'],
   driver: PostgreSqlDriver,
-  dbName: configService.get('DB_NAME'),
+  // dbName: configService.get('DB_NAME'),
+  dbName: 'Test',
+  password: 'postgres',
   user: configService.get('DB_USERNAME'),
-  password: configService.get('DB_PASSWORD'),
+  // password: configService.get('DB_PASSWORD'),
   host: configService.get('DB_HOST'),
   port: configService.get('DB_PORT'),
   debug: true,
   migrations: {
     path: './src/database/migrations',
   },
-} as Options;
+  seedes: {
+    path: './src/database/seeds',
+  },
+  extensions: [Migrator, EntityGenerator, SeedManager],
+} as Options<PostgreSqlDriver>);
