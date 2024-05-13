5d460c566950ad59f9dbb96419be11a81e54fd62
balaturgija <matej.balat@gmail.com>
2024-05-13 12:52:37 +0200

    Fix configuration

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,26 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import config from './mikro-orm.config';
+import { ConfigService } from '@nestjs/config';
+import { defineConfig } from '@mikro-orm/postgresql';
 
 @Module({
-  imports: [MikroOrmModule.forRoot()],
+  imports: [
+    MikroOrmModule.forRoot(),
+    MikroOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return defineConfig({
+          ...config,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          dbName: configService.get('DB_NAME'),
+          user: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          debug: true,
+        });
+      },
+    }),
+  ],
 })
 export class DatabaseModule {}
--- src/database/mikro-orm.config.ts
+++ src/database/mikro-orm.config.ts
@@ -2,26 +2,43 @@ import { Options } from '@mikro-orm/core';
 import { EntityGenerator } from '@mikro-orm/entity-generator';
 import { Migrator } from '@mikro-orm/migrations';
 import { defineConfig, PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { SeedManager } from '@mikro-orm/seeder';
-import { ConfigService } from '@nestjs/config';
+import dotenv from 'dotenv';
+import { AccountEntity } from 'src/accounts/entities/account.entity';
+import { AssetEntity } from 'src/assets/entities/asset.entity';
+import { OrderEntity } from 'src/orders/entities/orders.entity';
+import { PortfolioAssetEntity } from 'src/portfolios/entities/portfolio-asset.entity';
+import { PortfolioEntity } from 'src/portfolios/entities/portfolio.entity';
+import { WalletEntity } from 'src/wallets/entities/wallet.entity';
 
-const configService = new ConfigService();
+dotenv.config();
+
+const { DB_NAME, DB_PASSWORD, DB_USERNAME, DB_HOST, DB_PORT } = process.env;
 
 export default defineConfig({
   entities: ['./dist/*/entities/*.entity.js'],
-  entitiesTs: ['./src/*/entities/*.entity.ts'],
+  entitiesTs: [
+    AccountEntity,
+    WalletEntity,
+    AssetEntity,
+    OrderEntity,
+    PortfolioEntity,
+    PortfolioAssetEntity,
+  ],
   driver: PostgreSqlDriver,
-  dbName: 'Test',
-  password: 'postgres',
-  user: configService.get('DB_USERNAME'),
-  host: configService.get('DB_HOST'),
-  port: configService.get('DB_PORT'),
+  dbName: DB_NAME,
+  password: DB_PASSWORD,
+  user: DB_USERNAME,
+  host: DB_HOST,
+  port: parseInt(DB_PORT),
   debug: true,
   migrations: {
-    path: './src/database/migrations',
+    path: './dist/database/migrations',
+    pathTs: './src/database/migrations',
   },
-  seedes: {
-    path: './src/database/seeds',
-  },
+  seedes: { path: './dist/database/seeds', pathTs: './src/database/seeds' },
+  metadataCache: { options: { cacheDir: './src/database/metadata-cache' } },
+  metadataProvider: TsMorphMetadataProvider,
   extensions: [Migrator, EntityGenerator, SeedManager],
 } as Options<PostgreSqlDriver>);
